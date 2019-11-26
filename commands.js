59ab93357005a6b03140e3dceb9119379909deda
Luan Brito <lab7@cin.ufpe.br>
2019-11-26 11:02:45 -0300

    add mais queries

--- commands.js
+++ commands.js
@@ -7,11 +7,15 @@ db.Hospitau.renameCollection("Hospital")
 db.Hospital.insert({ nome: "Hospital da Restauracao", endereco: "Recife - PE" })
 
 db.Hospital.find({nome: "Hospital da Restauracao"}).pretty()
- 
+
 db.Hospital.update( {nome: "Hospital da Restauracao"}, {nome: "Hospital da Restauracao", endereco: "Recife - PE", atendimentos: ["Patricia", "Orlando", "Oswaldo"], anoInauguracao: 1984} )
 
 
 db.Hospital.insert({ nome: "Hospital da Luz", endereco: "Australia", atendimentos: ["Carlos", "Giovanna", "Eduardo"], anoInauguracao: 1995 })
+
+db.Hospital.insert({ nome: "Hospital PT", endereco: "Carpina", atendimentos: ["Dilma", "Lula", "Haddad", "Manuela"], anoInauguracao: 2000 })
+
+db.Hospital.insert({ nome: "Hospital Bed Stuy", endereco: "Brooklyn", atendimentos: ["Perigo", "Gregorio", "Chris", "Carlos"], anoInauguracao: 1994 })
 
 db.Hospital.aggregate([
    {
@@ -23,3 +27,13 @@ db.Hospital.aggregate([
       }
    }
 ])
+
+db.Hospital.aggregate([
+  {
+    $match: {
+      anoInauguracao: { $gte: 1990 }
+    }
+  }, {$sort: {"anoInauguracao": 1}}
+ ])
+
+db.Hospital.find({anoInauguracao: {$gte: 1990}}).sort({anoInauguracao: 1}).limit(1)
